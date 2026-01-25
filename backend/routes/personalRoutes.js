c8d6b170884fd5cef2f9f547d00b5a9d39e69dde
Aditya Shrivastava <as24csb1a04@student.nitw.ac.in>
2026-01-25 21:30:59 +0530

    Update personalRoutes.js

--- backend/routes/personalRoutes.js
+++ backend/routes/personalRoutes.js
@@ -1,1 +1,6 @@
-
+import express from "express";
+import { detailsController ,  educationController} from "../controllers/personalController.js";
+const personalRoutes = express.Router();
+personalRoutes.get('/details' , detailsController);
+personalRoutes.get('/education' , educationController);
+export default personalRoutes;
