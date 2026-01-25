4ab47fce968ea660651a9f9da16515191c9231a3
Aditya Shrivastava <as24csb1a04@student.nitw.ac.in>
2026-01-25 21:31:53 +0530

    Update workRoutes.js

--- backend/routes/workRoutes.js
+++ backend/routes/workRoutes.js
@@ -1,1 +1,9 @@
-
+import express from "express";
+import { skillscontroller , achievementscontroller , extracurricularcontroller } from "../controllers/workController.js";
+import { codingController } from "../controllers/codingController.js";
+const workRoutes = express.Router();
+workRoutes.get('/skills' , skillscontroller);
+workRoutes.get('/coding' , codingController);
+workRoutes.get('/achievements' , achievementscontroller);
+workRoutes.get('/extracurricular' , extracurricularcontroller);
+export default workRoutes;
